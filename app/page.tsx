9a71d592af02af0a04e1e40bf47cff94f1c7b5d7
Pritam12F <pritam.das.santu@gmail.com>
2024-06-08 00:04:23 +0530

    added split array func

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Icons } from "@/components/Icons";
 import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
 import { Phone } from "@/components/Phone";
+import { Reviews } from "@/components/Reviews";
 import { buttonVariants } from "@/components/ui/button";
 import { ArrowRight, Check, Star } from "lucide-react";
 import Link from "next/link";
@@ -197,7 +198,9 @@ export default function Home() {
           </div>
         </MaxWidthWrapper>
 
-        <div className="pt-16">{/* <Reviews /> */}</div>
+        <div className="pt-16">
+          <Reviews />
+        </div>
       </section>
 
       <section>
--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -1,1 +1,51 @@
-export const Reviews = () => {};
+"use client";
+import { useRef } from "react";
+import { MaxWidthWrapper } from "./MaxWidthWrapper";
+import { useInView } from "framer-motion";
+
+const PHONES = [
+  "/testimonials/1.jpg",
+  "/testimonials/2.jpg",
+  "/testimonials/3.jpg",
+  "/testimonials/4.jpg",
+  "/testimonials/5.jpg",
+  "/testimonials/6.jpg",
+];
+
+function splitArray<T>(array: Array<T>, columns: number) {
+  const result: Array<Array<T>> = [];
+  const perCol = Math.floor(array.length / columns);
+  for (let i = 0; i < array.length; i = i + perCol) {
+    const temp = [];
+    for (let j = i; j < i + perCol; ++j) {
+      temp.push(array[j]);
+    }
+    result.push(temp);
+  }
+
+  return result;
+}
+
+const ReviewGrid = () => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const inView = useInView(containerRef, { once: true, amount: 0.4 });
+  const cols = splitArray(PHONES, 3);
+
+  return (
+    <div
+      ref={containerRef}
+      className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"
+    ></div>
+  );
+};
+export const Reviews = () => {
+  return (
+    <MaxWidthWrapper className="relative max-w-5xl">
+      <img
+        aria-hidden="true"
+        src="/what-people-are-buying.png"
+        className="absolute select-none hidden xl:block -left-32 top-1/3"
+      />
+    </MaxWidthWrapper>
+  );
+};
